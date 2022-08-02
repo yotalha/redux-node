745b584e3e3ed4da99f8e95c5a4107d1610b1cf6
Talha Arshad <talha.arshad@systemsltd.com>
2022-08-02 19:50:56 +0500

    basics of redux

---
+++ index.js
@@ -0,0 +1,67 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
+
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOKED';
+
+
+function orderCake(){
+    return {
+        type: CAKE_ORDERED,
+        payload: 1
+    }
+}
+
+function restockCake(qty = 1){
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+    anotherProperty: 0
+}
+
+// (previousState, action) => newState
+
+// writing reducer
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes -1
+            }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer)
+console.log('initial state ', store.getState())
+
+const unsubscribe = store.subscribe(() => console.log('udpated state ', store.getState()))
+
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(3))
+
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
+
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(4)
+
+unsubscribe()
+
