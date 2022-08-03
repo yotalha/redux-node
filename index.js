0aff0d1c90f15eca101e1d61e011f486f7e21809
Talha Arshad <talha.arshad@systemsltd.com>
2022-08-03 13:21:44 +0500

    two users single reducer

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
 const CAKE_RESTOCKED = 'CAKE_RESTOKED';
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
 
 
 function orderCake(){
@@ -20,9 +22,25 @@ function restockCake(qty = 1){
     }
 }
 
+function orderIceCream(qty = 1){
+    return {
+        type: ICECREAM_ORDERED,
+        payload: qty
+    }
+}
+
+function restockIceCream(qty = 1){
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty
+    }
+}
+
+
+
 const initialState = {
     numOfCakes: 10,
-    anotherProperty: 0
+    numOfIceCreams: 20
 }
 
 // (previousState, action) => newState
@@ -41,6 +59,16 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes + action.payload
             }
+        case ICECREAM_ORDERED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - action.payload
+            }
+        case ICECREAM_RESTOCKED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams + action.payload
+            }
         default:
             return state;
     }
@@ -58,12 +86,16 @@ const unsubscribe = store.subscribe(() => console.log('udpated state ', store.ge
 
 // Not so useful now , all it does is it binds the functions to dispatch so the code looks a little cleaner
 
-const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
+const actions = bindActionCreators({ orderCake, restockCake, orderIceCream, restockIceCream }, store.dispatch)
 
 actions.orderCake()
 actions.orderCake()
 actions.orderCake()
 actions.restockCake(4)
+actions.orderIceCream(4)
+actions.restockIceCream(5)
+
+
 
 unsubscribe()
 
