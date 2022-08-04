1677a0b5057758f4e84194c8cbbd03aea868b71f
Talha Arshad <talha.arshad@systemsltd.com>
2022-08-04 13:32:45 +0500

    async redux, thunk middleware

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const redux = require('redux');
 const createStore = redux.createStore;
 const bindActionCreators = redux.bindActionCreators;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware
+
+const reduxLogger = require('redux-logger')
+const logger = reduxLogger.createLogger()
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
 const CAKE_RESTOCKED = 'CAKE_RESTOKED';
@@ -95,10 +99,10 @@ const rootReducer = combineReducers({
     iceCream: iceCreamReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('initial state ', store.getState())
 
-const unsubscribe = store.subscribe(() => console.log('udpated state ', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 
 // store.dispatch(orderCake())
 // store.dispatch(orderCake())
